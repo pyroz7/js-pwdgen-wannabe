22d9f5cfa9bc1196fa9ceb4786f94b760e7aebac
pyroz7 <vincipyroz7@gmail.com>
2023-11-09 20:32:23 +0100

    Half

---
+++ js/main.js
@@ -0,0 +1,37 @@
+//Informazioni da chiedere all'utente..
+
+let name = prompt ("Inserisci il tuo nome");
+let surname = prompt("Inserisci il tuo cognome");
+let colour = prompt("Inserisci il tuo colore preferito");
+let luckyNumber = prompt("Inserisci il tuo numero preferito");
+let luckyNumbertwo = prompt("Inserisci il tuo secondo numero preferito");
+
+
+
+
+
+
+
+
+
+
+// Compliazione dei dati
+let businessCard = `
+-------------- Il tuo bigliettino da visita --------------
+> Informazioni personali:
+Nome e Cognome: ${name} ${surname}
+Colore: ${colour}
+Numero preferito: ${21}
+Numero preferito due : ${7}
+`;
+
+
+
+console.log("-------------- Il tuo bigliettino da visita --------------");
+console.log("> Informazioni personali:");
+console.log("Nome e Cognome: " + name + " " + surname);
+console.log("Colore:" + colour );
+console.log("Numero preferito:" + luckyNumber);
+console.log("Numero preferito due:" + luckyNumbertwo);
+console.log("Totale somma numeri" + finalNumber)
+console.log("-------------------------------------------------------------");
